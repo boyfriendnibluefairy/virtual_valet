2b6c85d15264d297f5556dd15a316a37ea3993eb
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-08 12:06:21 +0100

    added sandbox main.py for running examples

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+    main.py is intentionally left blank so that you can easily cut-and-paste code from any of the scripts inside
+    ch01_user_interface/archived_examples/
+
+    The description of each script is presented in README.md
+"""
