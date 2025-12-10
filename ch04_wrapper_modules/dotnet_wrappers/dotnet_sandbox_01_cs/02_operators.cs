ba6f7ab3bccb94d29cd16abe87018b856c637bf9
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-10 11:59:18 +0100

    csharp basic operators

---
+++ ch04_wrapper_modules/dotnet_wrappers/dotnet_sandbox_01_cs/02_operators.cs
@@ -0,0 +1,19 @@
+﻿// sample values
+int a = 33, b = 5;
+string c = "hello", d = "world";
+
+Console.WriteLine("====== Math Operations ======");
+Console.WriteLine((a + b) / 2);
+Console.WriteLine(c + " " + d);
+
+Console.WriteLine("====== Increment and Decrement Operators ======");
+a++; b--;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine("====== Null Coalescing Operator ======");
+string x1 = null;
+string x2 = "violet";
+Console.WriteLine(x1 ?? "no value");
+Console.WriteLine(x2 ?? "Evergarden");
+Console.ReadKey();
