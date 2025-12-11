2c4caa2ec202f988fb87fa0f0396f23efc5a3587
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-11 12:28:49 +0100

    csharp comments and XML documentation

---
+++ ch04_wrapper_modules/dotnet_wrappers/dotnet_sandbox_01_cs/03_comments.cs
@@ -0,0 +1,16 @@
+﻿// this is a single line comment.
+
+/*
+ This is a
+ multi-line comment.
+*/
+
+/// XML Comments start with /// and are used to
+/// provide documentation.
+/// <summary>
+/// This void function demonstrates how to write XML comments.
+/// </summary>
+/// <return>
+/// No return value.
+/// </return>
+/// See microsoft.com on how to build projects with XML comments.
