5a91c251626eba925130331fa62f3106c5f6bb47
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-12 20:21:40 +0100

    csharp if, switch, ternary

---
+++ ch04_wrapper_modules/dotnet_wrappers/dotnet_sandbox_01_cs/04_decisions.cs
@@ -0,0 +1,39 @@
+﻿
+// sample value
+int focal_length = 100;
+
+// if else statement
+if (focal_length == 100)
+{
+    Console.WriteLine("focal length is 100");
+}
+else
+{
+    Console.WriteLine("focal length is is not 100.\n");
+}
+
+// if else can be replaced by ternary operator
+Console.WriteLine(focal_length < 100 ? "the focal length is less than 100" : "the focal length is 100 and above");
+
+
+// sample value
+int FOV = 53;
+
+// switch statement
+switch (FOV)
+{
+    case 00:
+    case 59:
+        Console.WriteLine("FOV is low");
+        break;
+    case 60:
+        Console.WriteLine("FOV is 60");
+        break;
+    case 61:
+    case 100:
+        Console.WriteLine("FOV is high");
+        break;
+    default:
+        Console.WriteLine("invalid FOV");
+        break;
+}
