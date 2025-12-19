92161b946ffe756b44af82906e02c37c1fde3a55
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-19 13:37:04 +0100

    csharp readline, writeline and randnext

---
+++ ch04_wrapper_modules/dotnet_wrappers/dotnet_sandbox_01_cs/07_readline_writeline_randnext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FineTuneRadiusOfCurvature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sInput = "";
+            int iInput = 0;
+            int answer = 0;
+            int guessCounter = 0;
+
+            // Generate Randon integer between 0 and 20
+            Random rand = new Random();
+            ground_truth_ROC = rand.Next(0, 100);
+            
+            Console.WriteLine("\n=================================");
+            Console.WriteLine("Ask PyTorch to optimize ROC!");
+            Console.WriteLine("PyTorch is thinking of number between 0 and 100");
+            Console.WriteLine("Type PyTorch guess, or -1 to give up");
+            Console.WriteLine("What's your guess?");
+            Console.WriteLine("=================================\n");
+
+            do
+            {
+                Console.WriteLine(" ");
+                Console.Write("---------> ");
+                sInput = Console.ReadLine();
+                guessCounter++;
+                if(int.TryParse(sInput, out iInput))
+                {
+                    if(iInput == ground_truth_ROC)
+                    {
+                        Console.WriteLine(" ");
+                        Console.WriteLine($"PyTorch got it in {guessCounter} guess(es)!!!");
+                    }
+                    else if (iInput < answer)
+                    {
+                        Console.WriteLine("Nope, higher than that.");
+                        Console.WriteLine("What's your guess?");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nope, lower than that.");
+                        Console.WriteLine("What's your guess?");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hmmm, that doesn't look like a number. Try again.");
+                    Console.WriteLine("What's your guess?");
+                }
+
+            } while (iInput != (-1));
+
+        }
+    }
+}
